c027c190bb64947ef93e52f1fdd330885d196d58
Carlos Torres <rocatoro18@outlook.com>
2022-11-21 20:36:37 -0700

    add splash activity

---
+++ app/src/main/java/com/example/musichub/SplashActivity.kt
@@ -0,0 +1,36 @@
+package com.example.musichub
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.annotation.RequiresApi
+
+class SplashActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.R)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        @Suppress("DEPRECATION")
+        Handler().postDelayed(
+            {
+                // Launch the main activity
+                startActivity(Intent(this@SplashActivity,MainActivity::class.java))
+                finish() // Call this when your activity is done and should be closed
+            },2500
+        )
+
+    }
+}
